859e35f1215f1baa62ec7a54b5c4a62c66e224f5
sriranjanijp <sriranjani.jp@gmail.com>
2024-06-04 21:09:14 +0530

    second

---
+++ multiplication.c
@@ -0,0 +1,17 @@
+int mul(int a, int b);
+
+void main()
+{
+    int y = 10;
+    int x = 5;
+    int c = mul(x,y);
+    
+    printf("The multiplication of %d and %d is %d\n",x,y,c);
+}
+
+int p;
+int mul(int a, int b)
+{
+    p = a*b;
+    return(p);
+}
