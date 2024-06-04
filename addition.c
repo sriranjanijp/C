153ba8cf1459bf6ab0d8b21bc35cd6b015d72d71
sriranjanijp <sriranjani.jp@gmail.com>
2024-06-04 20:22:53 +0530

    second

---
+++ addition.c
@@ -0,0 +1,14 @@
+void main()
+{
+    int number;
+    float amount;
+    float sum;
+
+    number = 100;
+    amount = 30.75;
+    sum = number + amount;
+
+    printf("%d\n",number);
+    printf("%5.2f\n",amount);
+    printf("%5.2f\n",sum);
+}
