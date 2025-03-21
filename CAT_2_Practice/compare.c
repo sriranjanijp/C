8fe6a87d29350cee7f877c39a24a8c16cc0e28f1
sriranjanijp <sriranjani.jp@gmail.com>
2025-03-21 22:02:06 +0530

    Attack on Bangtan - BTS

---
+++ CAT_2_Practice/compare.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s1[] = "abc";
+    char s2[] = "bcd";
+    printf("\n %d\n\n",strcmp(s2,s1));
+}
---
+++ CAT_2_Practice/dynamic.c
@@ -0,0 +1,36 @@
+//using malloc and calloc
+//Input list of numbers in range 0-4 and find frequency
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *num,*freq,i,size;
+
+    printf("Enter size of the list: ");
+    scanf("%d",&size);
+    num = (int *)malloc(size *sizeof(int));
+    printf("\nEnter the numbers: ");
+    for(i = 0;i<size;i++)
+    {
+        printf("\nEnter a number : ");
+        scanf("%d",&num[i]);
+        if(num[i]<0||num[i]>4)
+        {
+            printf("\nNumber should be in the 0-4 range");
+            i--;
+        }
+    }
+    freq = (int*)calloc(5,sizeof(int));
+    for(i=0;i<size;i++)
+    {
+        freq[num[i]]++;
+    }
+    printf("\nThe frequencies of the numbers: ");
+    for(i=0;i<5;i++)
+    {
+        printf("\nfreq[%d]: %d",i,freq[i]);
+    }
+    return 0;
+}
